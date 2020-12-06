f74c373a60614172f8dac9e0c0b4e0710d6fce2b
Samicap <lovestochasewindmills@gmail.com>
2020-12-06 17:38:37 +0000

    added pari programming files

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -6,8 +6,8 @@ const wordSearch = require('../wordsearch.js')
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],   // [ AWCFQUAL ]
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],  // [ SEINFELD ]
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
@@ -35,4 +35,36 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+});
+
+it("should return false if the word is not present (vertical)", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'COSMO');
+
+  assert.isFalse(result);
+});
+
+it("should return true if the word is present (vertical)", function() {
+  const result = wordSearch([
+    ['V', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['A', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['N', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['D', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['A', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['Y', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['S', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'VANDELAY');
+
+  assert.isTrue(result);
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
+// const loopJoin = function(array, word) {
+//     for (letter of array) {
+//         if (letter.includes(word)) return true;
+//     }
+// };
+
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (let letter of horizontalJoin) {
+        if (letter.includes(word)) return true;
+    } 
+    let newSearchMatrix = transpose(letters);
+    const verticalJoin = newSearchMatrix.map(ls => ls.join(''))
+    for (letter of verticalJoin) {
+        if (letter.includes(word)) return true;
     }
-}
+    return false;
 
-module.exports = wordSearch
+};
+
+let transpose = function(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    const grid = [];
+    for (let j = 0; j < cols; j++) {
+    grid[j] = Array(rows);
+    }
+    for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        grid[j][i] = matrix[i][j];
+    }
+    }
+    return grid;
+};
+
+module.exports = wordSearch;
