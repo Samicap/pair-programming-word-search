b98ba40c066e339ef5dfd9d8b298fc098ed65ff0
Samicap <lovestochasewindmills@gmail.com>
2020-12-06 17:48:38 +0000

    refactored pair prog  with Ryan MacFarlane

--- wordsearch.js
+++ wordsearch.js
@@ -1,37 +1,44 @@
-// const loopJoin = function(array, word) {
-//     for (letter of array) {
-//         if (letter.includes(word)) return true;
-//     }
-// };
+let _ = require('lodash');
+
+
+const loopJoin = function(array, word) {
+    for (letter of array) {
+        if (letter.includes(word)) return true;
+    }
+};
 
 
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (let letter of horizontalJoin) {
-        if (letter.includes(word)) return true;
-    } 
+    if (loopJoin(horizontalJoin, word) === true) {
+        return true;
+    };
+    
     let newSearchMatrix = transpose(letters);
-    const verticalJoin = newSearchMatrix.map(ls => ls.join(''))
-    for (letter of verticalJoin) {
-        if (letter.includes(word)) return true;
+    const verticalJoin = newSearchMatrix.map(ls => ls.join(''));
+    if(loopJoin(verticalJoin, word) === true) {
+        return true;
     }
     return false;
-
+    
+};
+const transpose = function(matrix) {
+    return _.zip(...matrix);
 };
 
-let transpose = function(matrix) {
-    const rows = matrix.length;
-    const cols = matrix[0].length;
-    const grid = [];
-    for (let j = 0; j < cols; j++) {
-    grid[j] = Array(rows);
-    }
-    for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-        grid[j][i] = matrix[i][j];
-    }
-    }
-    return grid;
-};
+// let transpose = function(matrix) {
+//     const rows = matrix.length;
+//     const cols = matrix[0].length;
+//     const grid = [];
+//     for (let j = 0; j < cols; j++) {
+//     grid[j] = Array(rows);
+//     }
+//     for (let i = 0; i < rows; i++) {
+//     for (let j = 0; j < cols; j++) {
+//         grid[j][i] = matrix[i][j];
+//     }
+//     }
+//     return grid;
+// };
 
 module.exports = wordSearch;
